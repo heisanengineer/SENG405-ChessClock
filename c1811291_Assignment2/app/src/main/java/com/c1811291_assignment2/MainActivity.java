2784147c1a0d8847dd62831042f8ad924b826752
Emre Can Aşık <ottomatic@windowslive.com>
2022-03-17 15:05:41 +0300

    Added some features

--- c1811291_Assignment2/app/src/main/java/com/c1811291_assignment2/MainActivity.java
+++ c1811291_Assignment2/app/src/main/java/com/c1811291_assignment2/MainActivity.java
@@ -1,23 +1,19 @@
 package com.c1811291_assignment2;
 import static java.lang.Integer.parseInt;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 
 
 public class MainActivity extends AppCompatActivity {
     Runnable runnable;
     Handler handler;
-
     EditText textinput;
     TextView textView1;
     TextView textView2;
@@ -34,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
     int minute=0;
     int whichPlayer=0;
     int j=1;
-    int maxMinute=0;
+    int maxMinute=5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,72 +52,32 @@ public class MainActivity extends AppCompatActivity {
         button.setEnabled(true);
         button2.setEnabled(true);
         button3.setEnabled(true);
-        button4.setEnabled(true);
+        button4.setEnabled(false);
         button5.setEnabled(true);
-
 
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                i++;
-                second=00;
-                minute=0;
-                textView1.setText(" "+second);
-                textView2.setText(" "+minute);
-
-                if (i == 1) {
-                    start(textView1);
-                    button.setText("STOP");
-                    button2.setText("START");
-                    whichPlayer=1;
-
-                } else if (i == 2) {
-                    stop(textView1);
-                    button.setText("START");
-                    button2.setText("STOP");
-                    i = 1;
-                    start2(textView1);
-                    whichPlayer=2;
-                }
+                player1();
             }
         });
 
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                i++;
-                second=0;
-                minute=0;
-                textView3.setText(""+second);
-                textView4.setText(""+minute);
-
-                if (i == 1) {
-                    start2(textView1);
-                    button.setText("START");
-                    button2.setText("STOP");
-                    whichPlayer=0;
-
-                } else if (i == 2) {
-                    stop(textView1);
-                    button2.setText("START");
-                    button.setText("STOP");
-                    i = 1;
-                    start(textView1);
-                    whichPlayer =1;
-                }
+                player2();
             }
         });
     }
 
     public void change(View view){
-
         maxMinute = parseInt(textinput.getText().toString());
-
+        button3.setText("CHANGED");
+        button3.setBackgroundColor(Color.BLACK);
     }
     public void start (View view)
     {
-
         handler = new Handler();
         runnable = new Runnable() {
             @Override
@@ -137,10 +94,13 @@ public class MainActivity extends AppCompatActivity {
                 textView2.setText(""+minute);
                 handler.postDelayed(runnable,1000);
             }else{
-                    Toast.makeText(getApplicationContext(),"TIME IS UP!",Toast.LENGTH_SHORT).show();
                     stop(textView1);
+                    button.setText("TIME IS UP!");
+                    button.setBackgroundColor(Color.BLACK);
+                    button2.setText("WIN!");
+                    button2.setBackgroundColor(Color.RED);
                 }}
-        };handler.post(runnable);
+        };handler.post(runnable); button4.setEnabled(true);
     }
 
     public void start2 (View view)
@@ -149,7 +109,6 @@ public class MainActivity extends AppCompatActivity {
         runnable = new Runnable() {
             @Override
             public void run() {
-
                 if(maxMinute>minute) {
                     textView3.setText("" + second);
                     textView4.setText("" + minute);
@@ -162,12 +121,14 @@ public class MainActivity extends AppCompatActivity {
                     textView4.setText("" + minute);
                     handler.postDelayed(runnable, 1000);
                 }else{
-                    Toast.makeText(getApplicationContext(),"TIME IS UP!",Toast.LENGTH_LONG).show();
                     stop(textView1);
+                    button2.setText("TIME IS UP!");
+                    button2.setBackgroundColor(Color.BLACK);
+                    button.setText("WIN!");
+                    button.setBackgroundColor(Color.RED);
                 }
             }
-        };handler.post(runnable);
-
+        };handler.post(runnable); button4.setEnabled(true);
     }
 
     public void pause(View view)
@@ -183,31 +144,75 @@ public class MainActivity extends AppCompatActivity {
             j=1;
 
             if (whichPlayer == 1) {
-                start(textView1);
+                start(view);
 
             } else if (whichPlayer == 2) {
-                start2(textView3);
+                start2(view);
             }
         }
-
     }
 
     public void stop(View view)
     {
+        button4.setEnabled(true);
         handler.removeCallbacks(runnable);
         second=0;
         minute=0;
-        button.setText("START");
-        button2.setText("START");
         i=0;
         button5.setText("PAUSE");
         textView1.setText(""+second);
         textView2.setText(""+minute);
         textView3.setText(""+second);
         textView4.setText(""+minute);
-        button3.setEnabled(true);
+        button.setText("START");
+        button2.setText("START");
+        button.setBackgroundColor(Color.rgb(255,87,34));
+        button2.setBackgroundColor(Color.rgb(103,58,183));
     }
 
+    public void player1(){
+        i++;
+        second=00;
+        minute=0;
+        textView1.setText(" "+second);
+        textView2.setText(" "+minute);
 
+        if (i == 1) {
+            start(textView1);
+            button.setText("STOP");
+            button2.setText("START");
+            whichPlayer=1;
 
+        } else if (i == 2) {
+            stop(textView1);
+            button.setText("START");
+            button2.setText("STOP");
+            i = 1;
+            start2(textView1);
+            whichPlayer=2;
+        }
+    }
+
+    public void player2(){
+        i++;
+        second=0;
+        minute=0;
+        textView3.setText(""+second);
+        textView4.setText(""+minute);
+
+        if (i == 1) {
+            start2(textView1);
+            button.setText("START");
+            button2.setText("STOP");
+            whichPlayer=0;
+
+        } else if (i == 2) {
+            stop(textView1);
+            button2.setText("START");
+            button.setText("STOP");
+            i = 1;
+            start(textView1);
+            whichPlayer =1;
+        }
+    }
 }
